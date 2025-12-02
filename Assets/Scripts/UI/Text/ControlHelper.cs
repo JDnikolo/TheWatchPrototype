5f14315a5b779dac8ed96ce3c356bbf164797443
Marton <j21marma@student.his.se>
2025-12-03 00:24:09 +0100

    Control helpers fix

--- Assets/Scripts/UI/Text/ControlHelper.cs
+++ Assets/Scripts/UI/Text/ControlHelper.cs
@@ -1,9 +1,11 @@
-﻿using Attributes;
+﻿using System;
+using Attributes;
 using Managers.Persistent;
 using Runtime.Automation;
 using Runtime.FrameUpdate;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Utilities;
 
 namespace UI.Text
 {
@@ -13,26 +15,30 @@ namespace UI.Text
 		, IBehaviourChecker
 #endif
 	{
-		[SerializeField] private string showControlsActionName = "ShowControls";
+		[SerializeField] private string helpActionName = "Help";
 		[SerializeField] [DisableInInspector] private ControlText[] texts;
 
 		public FrameUpdatePosition FrameUpdateOrder => FrameUpdatePosition.Default;
 
-		private InputAction m_showControlsAction;
+		private InputAction m_helpAction;
 		private bool m_enabled = true;
 		
 		public void OnFrameUpdate()
 		{
-			m_showControlsAction ??= InputManager.Instance.PlayerMap.GetAction(showControlsActionName);
-			if (m_showControlsAction.WasPressedThisFrame()) SetEnabled(!m_enabled);
+			m_helpAction ??= InputManager.Instance.PersistentGameMap.GetAction(helpActionName);
+			if (m_helpAction.WasPressedThisFrame()) SetEnabled(!m_enabled);
 		}
 
 		private void SetEnabled(bool enabled)
 		{
 			m_enabled = enabled;
-			for (var i = 0; i < texts.Length; i++) texts[i].enabled = m_enabled;
+			for (var i = 0; i < texts.Length; i++) texts[i].gameObject.SetActive(enabled);
 		}
-		
+
+		private void Start() => GameManager.Instance?.AddFrameUpdate(this);
+
+		private void OnDestroy() => GameManager.Instance?.RemoveFrameUpdate(this);
+
 #if UNITY_EDITOR
 		private BehaviorTester<ControlText> m_tester;
 
