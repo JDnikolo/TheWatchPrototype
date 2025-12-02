9ca84d24dec2b4eea8ad23a2eb9b088fa03646e4
JDnikolo <j.nikolo@gmail.com>
2025-12-02 21:28:36 +0100

    Cinematic behavior added to intro

--- Assets/Scripts/Animation/PersonAnimationController.cs
+++ Assets/Scripts/Animation/PersonAnimationController.cs
@@ -176,7 +176,7 @@ namespace Animation
       private void OnDestroy()
       { 
          GameManager.Instance.RemoveFixedUpdateSafe(this);
-         PauseManager.Instance.RemovePausedCallback(this);
+         PauseManager.Instance?.RemovePausedCallback(this);
 
       }
 
