8d2124533b55dc711b3ffad5ac9440e2593e7be2
Marton <j21marma@student.his.se>
2025-12-08 10:44:33 +0100

    Disable forced

--- Assets/Scripts/Interactables/Actions/Agent/InteractableAgent.cs
+++ Assets/Scripts/Interactables/Actions/Agent/InteractableAgent.cs
@@ -6,7 +6,7 @@ namespace Interactables.Actions.Agent
 {
 	public abstract class InteractableAgent : Interactable
 	{
-		[SerializeField, AutoAssigned(AssignMode.Self | AssignMode.Parent | AssignMode.Forced, typeof(AgentBrain))]
+		[SerializeField, AutoAssigned(AssignMode.Self | AssignMode.Parent, typeof(AgentBrain))]
 		private AgentBrain agent;
 
 		protected AgentBrain Agent => agent;
