c9be40a167b07dc04ecda7562eda14edf220a65a
JDnikolo <j.nikolo@gmail.com>
2025-12-08 18:35:52 +0100

    Add pushables

--- Assets/Scripts/Interactables/Actions/Physics/InteractablePushSelf.cs
+++ Assets/Scripts/Interactables/Actions/Physics/InteractablePushSelf.cs
@@ -9,14 +9,13 @@ namespace Interactables.Actions.Physics
         [SerializeField] [AutoAssigned(AssignModeFlags.Parent | AssignModeFlags.Self, typeof(Rigidbody))]
         private Rigidbody selfRigidbody;
         
-        [SerializeField] private float force = 1;
+        [SerializeField] private float force = 10;
 
         public override void Interact()
         {
             var direction = (selfRigidbody.transform.position - 
-                             PlayerManager.Instance.PlayerCamera.transform.position.normalized);
+                             PlayerManager.Instance.PlayerCamera.transform.position).normalized;
             if (selfRigidbody.isKinematic) selfRigidbody.isKinematic = false;
-            Debug.Log(force * direction);
             selfRigidbody.AddForce(direction * force, ForceMode.Impulse);
         }
     }
