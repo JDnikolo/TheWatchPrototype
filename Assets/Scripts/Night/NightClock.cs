a72d0f10fee6dea35808089ade844353dd1c09c4
JDnikolo <j.nikolo@gmail.com>
2026-01-10 22:21:21 +0100

    Add new clocktower

--- Assets/Scripts/Night/NightClock.cs
+++ Assets/Scripts/Night/NightClock.cs
@@ -16,13 +16,13 @@ namespace Night
 		{
 			RotateTo(secondHandle, currentTime.second / 60f);
 			RotateTo(minuteHandle, currentTime.minute / 60f);
-			RotateTo(hourHandle, currentTime.hour / 24f);
+			RotateTo(hourHandle, currentTime.hour / 12f);
 		}
 		
 		private static void RotateTo(Transform transform, float partOfWhole)
 		{
 			var localEuler = transform.localEulerAngles;
-			localEuler.z = -(partOfWhole * 360f);
+			localEuler.z = (partOfWhole * 360f);
 			transform.localEulerAngles = localEuler;
 		}
 
